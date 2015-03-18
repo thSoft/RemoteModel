ea1197c5a0268d128c22d301a885e31e621fdfd9
Dénes Harmath <harmathdenes@gmail.com>
2015-03-18 14:18:06 +0100

    Remove deleted values from cache

--- src/main/Cache.elm
+++ src/main/Cache.elm
@@ -10,7 +10,7 @@ type alias Cache a = Dict String a
 
 type alias Entry a = Maybe {
   url: String,
-  value: a
+  value: Maybe a
 }
 
 loadCache : (a -> b) -> Signal (Entry a) -> Signal (Cache b)
@@ -20,7 +20,10 @@ updateCache : (a -> b) -> Entry a -> Cache b -> Cache b
 updateCache transform entry cache =
   case entry of
     Nothing -> cache
-    Just { url, value } -> cache |> insert url (value |> transform)
+    Just { url, value } ->
+      case value of 
+        Just realValue -> cache |> insert url (realValue |> transform)
+        Nothing -> cache |> remove url
 
 type alias Reference a = {
   url: String,
